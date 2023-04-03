f258d8c77fd2f6e43e8883e74a60c7f7ee9e3114
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-04-03 15:24:25 +0530

    added package info and removed mongoose error

--- app.js
+++ app.js
@@ -17,11 +17,7 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 
 //setting up mongodb
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-)
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("DB Connection Successful!")
 })
